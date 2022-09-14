02572d0b8cbfb6ddf20226543ba16a81a8a46f45
Swayee2022 <98569480+Deagle-PrintStream@users.noreply.github.com>
2022-09-14 15:24:42 +0800

    修改了tracer的类结构
    定义了sensor_t 的类，向上组成tracer_t的类，并将此类暂定为tracer_t_new，原有类定义尚未删除，原先的第二层算法的判断没有改变，需要再优化

--- Src/tracer.cpp
+++ Src/tracer.cpp
@@ -3,12 +3,243 @@
 #include "tracer.h"
 /* Exported macro ------------------------------------------------------------*/
 
-tracer_t tracer[4];
+//tracer_t tracer[directionCount];
+tracer_t_new tracer[directionCount];
 
 /* Exported functions definens ---------------------------------------------*/
 
 
 /* Class construction & destruction functions defines ---------------------------------------------*/
+
+
+
+/*sensor_t construction &destruction func*/
+sensor_t::sensor_t(float newCoe):valThrehold(valThreholdDefault)
+{
+	clearData();
+	setConfCoe(newCoe);
+}
+
+sensor_t::~sensor_t()
+{
+	clearData();
+}
+
+/*sensor_t private func*/
+
+void sensor_t::getNewVal(GPIO_TypeDef *gpioPort,uint16_t pin){
+  newSensorVal=(HAL_GPIO_ReadPin(gpioPort,pin)==blackParcel);
+}
+
+void sensor_t::updateCache(void){
+  valCache[cachePtr]=newSensorVal;
+  cachePtr++;
+  if(cachePtr>=valCacheMaxDefault){
+    cachePtr=0;
+  }
+}
+
+void sensor_t::updateValAverage(void){
+  for(uint8_t i=0;i<valCacheMaxDefault;i++){
+    valAverage+=valCache[i];
+  }
+  valAverage/=valCacheMaxDefault;
+}
+
+void sensor_t::updateValBinary(void){
+  valBinary=(valAverage>=valThrehold);
+}
+
+void sensor_t::clearCache(void){
+  for(uint8_t i=0;i<valCacheMaxDefault;i++){
+    valCache[i]=0;
+  }
+  cachePtr=0;
+}
+
+/*sensor_t public func*/
+
+status_t sensor_t::updateData(GPIO_TypeDef *gpioPort,uint16_t pin){
+  getNewVal(gpioPort,pin);
+  updateCache();
+  updateValAverage();
+  updateValBinary();
+  return getValBinary();
+}
+
+status_t sensor_t::getValBinary(void)const{
+  return valBinary;
+}
+
+void sensor_t::setConfCoe(float newCoe){
+  if(newCoe<=1 &&newCoe>=0)
+    confidenceCoe=newCoe;
+}
+
+void sensor_t::clearData(void){
+  newSensorVal=0;
+  valAverage=0;
+  valBinary=0;
+  clearCache();
+}
+
+/*tracer_t construction &destruction func*/
+
+tracer_t_new::tracer_t_new(){
+	clearData();
+}
+
+tracer_t_new::~tracer_t_new(){
+	clearData();
+}
+
+/*tracer_t private func*/
+
+status_t tracer_t_new::sensorVal(uint8_t order)const{
+  return sensor[order].getValBinary();
+}
+
+void tracer_t_new::updateSensorVal(void){
+  #define PORT(__order__) (PIN_SENSOR_##__order__##_GPIO_Port)
+  #define PIN(__order__)  (PIN_SENSOR_##__order__##_Pin)
+  using namespace tracer_nsp;
+  sensor[L2].updateData(PORT(0),PIN(0));
+  sensor[L1].updateData(PORT(1),PIN(1));
+  sensor[M].updateData(PORT(2),PIN(2));
+  sensor[R1].updateData(PORT(3),PIN(3));
+  sensor[R2].updateData(PORT(4),PIN(4));
+
+  #undef PORT
+  #undef PIN
+}
+
+void tracer_t_new::updatePathStatus(void){
+  using namespace tracer_nsp;
+
+  status_t preOnPath=onPath;
+  if(sensorVal(R1)+sensorVal(M)+sensorVal(L1)==3){
+    
+    onPath=1;
+  }else{
+    onPath=0;
+  }
+
+  if(preOnPath==0){
+    if(onPath==1){
+      hitPath[dirFront]=1;
+    }else{
+      hitPath[dirFront]=0;
+    }
+    if(sensorVal(R2)==1){
+      hitPath[dirRight]=1;
+    }else{
+      hitPath[dirRight]=0;
+    }
+    if(sensorVal(L2)==1){
+      hitPath[dirRight]=1;
+    }else{
+      hitPath[dirRight]=0;
+    }
+  }else if(preOnPath==1){
+    if(onPath==0){
+      leavePath[dirFront]=1;
+    }else{
+      leavePath[dirFront]=0;
+    }
+    if(sensorVal(R2)==1){
+      leavePath[dirLeft]=1;
+    }else{
+      leavePath[dirLeft]=0;
+    }
+    if(sensorVal(L2)==1){
+      leavePath[dirRight]=1;
+    }else{
+      leavePath[dirRight]=0;
+    }
+  }
+
+}
+
+void tracer_t_new::clearSensorVal(void){
+  for(uint8_t i=0;i<sensorCount;i++){
+    sensor[i].clearData();
+  }
+}
+
+void tracer_t_new::clearStatus(void){
+	onPath=0;
+	for(uint8_t i=0;i<3;i++){
+		hitPath[i]=0;
+		leavePath[i]=0;
+	}
+}
+
+/*tracer_t public func*/
+
+void tracer_t_new::updateData(void){
+  if(detectOn==on){
+    updateSensorVal();
+  }
+  if(calcStatus==on){
+    updatePathStatus();
+  }  
+
+}
+
+void tracer_t_new::detectMode(status_t newStatus){
+  /*默认传入参数为2，即为切换当前模式*/
+  if(newStatus>=2){
+    detectOn=!detectOn;
+  }else{
+    detectOn=newStatus;
+  }
+  clearSensorVal();
+}
+
+void tracer_t_new::calcStatusMode(status_t newStatus){
+  /*默认传入参数为2，即为切换当前模式*/
+  if(newStatus>=2){
+    calcStatus=!calcStatus;
+  }else{
+    calcStatus=newStatus;
+  }
+  clearStatus();
+}
+
+void tracer_t_new::setConfCoe(uint8_t sensorOrder,float newConfCoeVal){
+  if(sensorOrder<sensorCount){
+    sensor[sensorOrder].setConfCoe(newConfCoeVal);
+  }
+}
+
+status_t tracer_t_new::getPathStatus(hit_leave_t newStatus, direction_t newDir)const{
+  using namespace tracer_nsp;
+  /*只有3个情况*/
+  if(newDir==dirBack || newDir==dirNowhere)
+    return (status_t)(-1);
+
+  if(newStatus==leave){
+    if(newDir==dirAll){
+      return leavePath[dirFront]+leavePath[dirRight]+leavePath[dirLeft];
+    }else{
+      return leavePath[newDir];
+    }
+  }else if(newStatus==hit){
+    if(newDir==dirAll){
+      return hitPath[dirFront]+hitPath[dirRight]+hitPath[dirLeft];
+    }else{
+      return hitPath[newDir];
+    }
+  }
+  return (status_t)(-1);
+}
+
+void tracer_t_new::clearData(void){
+  clearSensorVal();
+  clearStatus();
+}
+
+
 /*构造函数*/
 tracer_t::tracer_t()\
 	:valThrehold(valThreholdDefault),confidenceCoeMax(confidenceCoeMaxDefault)
